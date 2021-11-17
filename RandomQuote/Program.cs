848c21aa9a31efe82a9f857f6da5e50718aa3e30
CarolyAruvel <89841849+CarolyAruvel@users.noreply.github.com>
2021-11-17 10:20:13 +0200

    Update Program.cs

--- RandomQuote/Program.cs
+++ RandomQuote/Program.cs
@@ -6,7 +6,15 @@ namespace RandomQuote
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string filePath = @"C:\Users\opilane\samples\RandomQuote.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            Console.WriteLine(dataFromFile[0]);
+            Random rnd = new Random();
+            int RandomIndex = rnd.Next(1, dataFromFile.Length);
+            Console.WriteLine(dataFromFile[RandomIndex]);
+        
         }
     }
 }
