aadd1bbbbbe08c4a30f7b6eec9ca74b1e67ab0c0
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:18:06 +0200

    Week12

---
+++ BlackSheep/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace BlackSheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Kirjuta programm, mis loeb faili sisu maha, asendab kõik numbrid tähtedega.
+            //Uuenda faili sisu, et salvestada sinna uuendatud teksti versiooni.
+
+            string filepath = @"C:\Users\opilane\samples\BlackSheep.txt";
+            string[] dataFromFile = File.ReadAllLines(filepath);
+            for (int i = 0; i < dataFromFile.Length; i++)
+            {
+                dataFromFile[i] = dataFromFile[i].Replace('4', 'a');
+                dataFromFile[i] = dataFromFile[i].Replace('3', 'e');
+                dataFromFile[i] = dataFromFile[i].Replace('0', 'o');
+                dataFromFile[i] = dataFromFile[i].Replace('1', 'i');
+            }
+               
+            foreach (string line in dataFromFile)
+            {
+               Console.WriteLine(line);
+            }
+
+            //update the file content
+            File.WriteAllLines(filepath, dataFromFile);
+             
+        }
+
+
+    }
+        
+    
+}
---
+++ Seasons/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string winterFile = @"C:\Users\opilane\samples\Winter.txt";
+            string autumnFile = @"C:\Users\opilane\samples\Autumn.txt";
+
+            string[] autumnData = File.ReadAllLines(autumnFile);
+            string[] winterData = File.ReadAllLines(winterFile);
+
+            File.WriteAllLines(autumnFile, autumnData);
+            File.WriteAllLines(winterFile, winterData);
+        }
+    }
+}
